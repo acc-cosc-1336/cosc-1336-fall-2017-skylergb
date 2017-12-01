7f1cf806cd287a5b1a35ad44eb9d8b88fa67705a
skylergb <31975400+skylergb@users.noreply.github.com>
2017-11-30 21:00:00 -0600

    Add files via upload

---
+++ contoso_app.py
@@ -0,0 +1,42 @@
+from tkinter import Tk, Frame
+from GUI.main_menu import MainMenu
+from GUI.frames.aboutframe import AboutFrame
+from GUI.frames.courseframe import CourseFrame
+from GUI.frames.enrollmentframe import EnrollmentFrame
+from GUI.frames.studentframe import StudentFrame
+
+class ContosoApp(Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        Tk.__init__(self, *args, **kwargs)
+
+        Tk.wm_title(self, 'Contoso university')
+        self.menubar = MainMenu(self)
+        self.config(menu=self.menubar)
+
+        self.frames = {}
+        self.__init_frames()
+
+    def __init_frames(self):
+
+        container = Frame(self)
+        container.pack(side='top', fill='both', expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        for frame in (AboutFrame, CourseFrame, EnrollmentFrame, StudentFrame):
+            current_frame = frame(container)
+            self.frames[frame.__name__] = current_frame
+            current_frame.grid(row=0, column=0, sticky='nsew')
+
+    def show_frame(self,frame_name):
+        frame = self.frames[frame_name]
+        Tk.wm_title(self, 'Contoso University - ' + frame_name.replace('Frame', ''))
+        frame.tkraise()
+                    
+                    
+
+app = ContosoApp()
+app.geometry('640x400')
+app.mainloop()
